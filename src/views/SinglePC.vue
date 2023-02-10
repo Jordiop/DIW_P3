1eb493f0c779f9114cd305317206d814b190c96b
Jordi Osarenkhoe Petro <jordiosarenkhoe@outlook.es>
2023-02-10 11:53:08 +0100

    update: footer and db

--- src/views/SinglePC.vue
+++ src/views/SinglePC.vue
@@ -4,33 +4,32 @@
     var url = "http://localhost:3000/pc/";
     export default {
         name: "SinglePC",
-        data() {
-            console.log(pcs)
-            return {
-                pcs: [],
-            }
-        },
         mounted() {
             id = this.$route.params.id;
             let fetchUrl = url + id;
             fetch(fetchUrl)
                 .then(response => response.json())
-                .then(data => {7
+                .then(data => {
                     this.pcs = data
                     pcs.push(data)
                 })
-        }
+        },
+        data() {
+            return {
+                pcs: [],
+            }
+        },
     }
 
 </script>
 <template>
-    <div v-for="comp in pcs" :key="comp.id" class="mt-32 grid grid-cols-2">
+    <div v-for="pc in pcs" class="mt-32 grid grid-cols-2">
         <div>
-            <img :src="comp.img" alt="PC Photo" />
+            <img :src="pc.img" alt="PC Photo" />
         </div>
         <div>
-            <h2 class="text-slate-700">{{ comp.name }}</h2>
-            <p class="mt-1 text-sm text-slate-400">{{ comp.description }}</p>
+            <h2 class="text-slate-700">{{ pc.name }}</h2>
+            <p class="mt-1 text-sm text-slate-400">{{ pc.description }}</p>
         </div>
     </div>
 </template>
