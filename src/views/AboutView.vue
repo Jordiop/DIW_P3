035dcb8c2643b8d45eaeebf7194d16b11703c0d7
Jordi Osarenkhoe Petro <jordiosarenkhoe@outlook.es>
2023-02-15 00:32:51 +0100

    update: db and about view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,80 @@
+<script setup>
+function postPresupuesto() {
+    let email = document.getElementById("email").value;
+    let nombre = document.getElementById("nombre").value;
+    let presupuesto = document.getElementById("presupuesto").value;
+    let uso = document.getElementById("uso").value;
+    let peticiones = document.getElementById("comentario").value;
+    let estado = "pendiente";
+    //Buscamos si el email ya existe
+    fetch("http://localhost:3000/presupuesto/")
+        .then(response => response.json())
+        .then(data => {
+            for (let i = 0; i < data.length; i++) {
+                if (data[i].email == email) {
+                    alert("El email ya existe");
+                    return;
+                }
+            }
+            //Si no existe, lo añadimos
+            fetch("http://localhost:3000/presupuesto", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        email: email,
+                        name: nombre,
+                        presupuesto: presupuesto,
+                        uso: uso,
+                        peticiones: peticiones,
+                        estado: estado
+                    }),
+                })
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log("Success:", data);
+                })
+                .catch((error) => {
+                    console.error("Error:", error);
+                });
+        })
+}
+
+function borrar(id){
+  fetch("http://localhost:3000/presupuesto/"+id, {
+    method: "DELETE",
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("Success:", data);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
+}
+
+function buscarPresupuesto(){
+    let email = document.getElementById("buscarEmail").value;
+    fetch("http://localhost:3000/presupuesto/")
+    .then(response => response.json())
+    .then(data => {
+      for (let i = 0; i < data.length; i++) {
+        if(data[i].email == email){
+          document.getElementById("resultadoBusqueda").innerHTML = (
+            "<p>Nombre: "+data[i].name+"</p>"+
+            "<p>Presupuesto: "+data[i].presupuesto+"</p>"+
+            "<p>Uso: "+data[i].uso+"</p>"+
+            "<p>Comentario: "+data[i].peticiones+"</p>"+
+            "<p>Estado: "+data[i].estado+"</p>" +
+            "<button class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' type='button' @click='borrar("+data[i].id+")'>Eliminar</button>"      
+          )
+        }
+      }
+    })
+}
+
+</script>
 <template>
   <div class="grid grid-cols-2 mt-20">
     <!--Upload your build-->
@@ -14,60 +91,18 @@
         <input class="border-2 border-gray-300 p-2 rounded-lg m-2" type="text" id="uso" placeholder="Uso">
         <label for="Comentario">Peticiones especiales</label>
         <input class="border-2 border-gray-300 p-2 rounded-lg m-2" type="text" id="comentario" placeholder="Comentario">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="button" @click="postPresupuesto()">Enviar</button>
       </form>
     </div>
     <div class="flex justify-center flex-col">
       <h1 class="text-xl text-center bold">Buscador de presupuestos</h1>
       <label for="buscarEmail">Email</label>
       <input class="border-2 border-gray-300 p-2 rounded-lg m-2" type="email" id="buscarEmail" placeholder="Email" />
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="button" @click="buscarPresupuesto()">Buscar</button>
+      <div id="resultadoBusqueda"></div>
     </div>
   </div>
 
 </template>
-<script>
-import { onMounted } from 'vue';
 
-async function postBuild(){
-    fetch('http://localhost:3000/builds', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            procesador: document.getElementById("procesador").value,
-            placaBase: document.getElementById("placaBase").value,
-            memoriaRAM: document.getElementById("memoriaRAM").value,
-            tarjetaGrafica: document.getElementById("tarjetaGrafica").value,
-            discoDuro: document.getElementById("discoDuro").value,
-            fuenteAlimentacion: document.getElementById("fuenteAlimentacion").value,
-            caja: document.getElementById("caja").value
-        })
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-    })
-    .catch(error => {
-        console.log(error)
-    })
-}
-const items = [];
-export default {
-    name: 'AboutView',
-    data() {
-        return {
-            items: [],
-        }
-    },
-    mounted(){
-            fetch("http://localhost:3000/builds")
-                .then(response => response.json())
-                .then(data => {
-                    this.items = data
-                })
-        },
-}
-
-
-</script>
 
